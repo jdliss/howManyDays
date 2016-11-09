c3a63bfb5d9454de48f96f21a15916173d8cba26
Jonathan <jonacom@lissismore.com>
2016-11-08 21:06:41 -0500

    done for the day

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-var app = new Vue({
+function getDaysBetween(date1, date2) {
+  if (date1.toDateString() === date2.toDateString()) { return 0; }
+  var one_day=1000*60*60*24;
+  var difference_ms = date2.getTime() - date1.getTime();
+  return Math.round(difference_ms/one_day); 
+}
+
+var prompt = new Vue({
   el: '#prompt',
   data: {
-    message: 'The date I\'m looking for is: ',
-    prompt: '12/25/2005'
+    message: 'How many days until ',
+    date: new Date().toLocaleDateString()
+  }
+});
+
+var daysLeft = new Vue({
+  el: "#days-left",
+  data: {
+    message: getDaysBetween(new Date(), new Date(prompt.date)) + " days left."
   }
 });
